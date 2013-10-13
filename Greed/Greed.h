b710e36fd7fbbaf927668f73bd2d2af1f64f987e
Mark Haskamp <mark.haskamp@pinnsg.com>
2013-10-13 16:34:44 -0400

    Initial Commit

---
+++ Greed/Greed.h
@@ -0,0 +1,13 @@
+//
+//  Greed.h
+//  Greed
+//
+//  Created by Mark Haskamp on 10/13/13.
+//  Copyright (c) 2013 Mark Haskamp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Greed : NSObject
+
+@end
---
+++ Greed/Greed.m
@@ -0,0 +1,13 @@
+//
+//  Greed.m
+//  Greed
+//
+//  Created by Mark Haskamp on 10/13/13.
+//  Copyright (c) 2013 Mark Haskamp. All rights reserved.
+//
+
+#import "Greed.h"
+
+@implementation Greed
+
+@end
